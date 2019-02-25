32b6c338c0f460ad2783a35f4bfd46e13eb660f4
allenchen <allenchen@yeahka.com>
2019-02-25 12:53:15 +0800

    脚本配置初始化

---
+++ src/main/java/com/test/App.java
@@ -0,0 +1,10 @@
+package com.test;
+
+public class App {
+
+	public static void main(String[] args) {
+		for(int i = 0 ; i<100 ; i++) {
+			System.out.println("java-script is run number = " + i);
+		}
+	}
+}
